0329cc056cc2f92cc719bb441ee98175c3bf6f8a
ilyasramadhann <ilyasramadhan.i.r.39@gmail.com>
2023-06-03 02:05:54 +0700

    adding countdown

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,13 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
-public class GamePanel extends JFrame  {
-    public final static int width = 512;    // screen width
-    public final static int height = 640;   // screen height
+public class GamePanel extends JFrame {
+    public final static int width = 512;    // lebar layar
+    public final static int height = 640;   // tinggi layar
     private JLabel bg;
     private JPanel mainPanel;
     private Environment env;
+
+    private JLabel countdownLabel;
+    private int countdownValue = 4;
+    private Timer countdownTimer;
+    private JButton startButton;
 
     public GamePanel() {
         setTitle("Lampung: Discover the best road");
@@ -18,7 +25,12 @@ public class GamePanel extends JFrame  {
         setResizable(false);
 
         GameManager gm = new GameManager();
-        
+
+        ImageIcon bgTitle = new ImageIcon("src/assets/title.png");
+        JLabel labelTitle = new JLabel(bgTitle);
+        labelTitle.setBounds(77, 107, 359, 309);
+        add(labelTitle);
+
         ImageIcon bgImage = new ImageIcon("src/assets/background.jpg");
         bg = new JLabel(bgImage);
         bg.setBounds(0, 0, 512, 640);
@@ -30,12 +42,12 @@ public class GamePanel extends JFrame  {
         mainPanel.setLayout(null);
         bg.add(mainPanel);
 
-        JButton startButton = new JButton();
+        startButton = new JButton();
         startButton.setBounds(129, 480, 255, 62);
         startButton.setText("Start The Game");
         startButton.setForeground(Color.white);
         startButton.setBackground(Color.green);
-        startButton.setFont(new java.awt.Font("Arial", Font.BOLD, 25));
+        startButton.setFont(new Font("Arial", Font.BOLD, 25));
         startButton.setBorder(null);
         startButton.setFocusPainted(false);
         startButton.setOpaque(true);
@@ -43,12 +55,48 @@ public class GamePanel extends JFrame  {
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                startGame();
+                startButton.setBounds(0, 0, 0, 0);
+                labelTitle.setVisible(false);
+                startCountdown();
             }
         });
+
+        // add font bold
+        try {
+            Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/assets/Poppins-Bold.ttf"));
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(customFont);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+
+        countdownLabel = new JLabel();
+        countdownLabel.setBounds(0, 250, width, 100);
+        countdownLabel.setFont(new Font("Poppins", Font.BOLD, 90));
+        countdownLabel.setForeground(new Color(253, 187, 60));
+        countdownLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        mainPanel.add(countdownLabel);
+
         setVisible(true);
     }
-    
+
+    private void startCountdown() {
+        countdownTimer = new Timer(1250, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                countdownValue--;
+                countdownLabel.setText(String.valueOf(countdownValue));
+                if (countdownValue < 0) {
+                    countdownTimer.stop();
+                    startGame();
+                } else if (countdownValue == 0) {
+                    countdownLabel.setText("GO!");
+                }
+            }
+        });
+        countdownTimer.start();
+    }
+
     private void startGame() {
         env = new Environment();
         env.setSize(width, height);
@@ -57,5 +105,4 @@ public class GamePanel extends JFrame  {
 
         bg.setVisible(false);
     }
-    
 }
